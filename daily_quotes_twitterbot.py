bb8d0b070e655d1b58dca673105dc26834bb833f
JS <51195305+sombra2@users.noreply.github.com>
2023-08-15 17:36:42 +0100

    update to fit Twitter's API v.2.0

--- daily_quotes_twitterbot.py
+++ daily_quotes_twitterbot.py
@@ -20,12 +20,14 @@ access_token = credentials.access_token
 access_token_secret = credentials.access_token_secret
 
 # authentication of consumer key and secret
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+client = tweepy.Client(
+                    bearer_token=credentials.bearer_token,
+                    consumer_key=credentials.consumer_key,
+                    consumer_secret=credentials.consumer_secret,
+                    access_token=credentials.access_token,
+                    access_token_secret=credentials.access_token_secret
+                    )
 
-# authentication of access token and secret
-auth.set_access_token(access_token, access_token_secret)
-api = tweepy.API(auth)
 
 # sending the tweet
-
-api.update_status(status = quote + ' #DailyQuote')
+client.create_tweet(text = quote + ' #DailyQuote')
